472e9e2265ffcaa7b8f41050fcbe9b9735b4cbc2
jeffersonfreitas21 <jefferson.dev21@gmail.com>
2021-01-22 11:15:21 -0300

    Primeiro commit

---
+++ src/test/java/dev/jeffersonfreitas/myfinance/MyfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jeffersonfreitas.myfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
